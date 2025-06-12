a447a12e925fa7c0ecec1cf2ac8587adb17149ae
Jordan <kamurasijordanarthur@gmail.com>
2025-06-12 12:30:44 +0300

    Deleted old project state

--- RF-client/GNU ARM v12.2.1 - Default/gecko_sdk_4.4.6/util/third_party/printf/printf.d
+++
@@ -1,4 +0,0 @@
-gecko_sdk_4.4.6/util/third_party/printf/printf.o: \
- /home/jordan/SimplicityStudio/SDKs/gecko_sdk/util/third_party/printf/printf.c \
- /home/jordan/SimplicityStudio/SDKs/gecko_sdk/util/third_party/printf/printf.h
-/home/jordan/SimplicityStudio/SDKs/gecko_sdk/util/third_party/printf/printf.h:
