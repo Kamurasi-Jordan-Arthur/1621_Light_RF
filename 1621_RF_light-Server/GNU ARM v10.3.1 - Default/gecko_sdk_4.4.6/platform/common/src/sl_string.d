68ea4f5a30bf06bbfee8e3f1e4c22cb682fb1963
Kamurasi Jordan Arthur <jordan.kamurasi@anfaengineering.com>
2025-07-18 10:40:06 +0300

    Upated ignore file to leavout unwanted files

--- 1621_RF_light-Server/GNU ARM v10.3.1 - Default/gecko_sdk_4.4.6/platform/common/src/sl_string.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.4.6/platform/common/src/sl_string.o: \
- C:/SiliconLabs/SimplicityStudio/v5/offline/sdk/gecko-sdk/platform/common/src/sl_string.c \
- c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\inc\sl_string.h
-c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\inc\sl_string.h:
--- bt-remote-Redesign/GNU ARM v10.3.1 - Default/QS/qs/qstamp.d
+++
@@ -1,5 +0,0 @@
-QS/qs/qstamp.o: ../QS/qs/qstamp.c \
- c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- C:\qp\qpc/include/qstamp.h
-c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-C:\qp\qpc/include/qstamp.h:
