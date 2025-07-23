95f6771f2f70c0f6d2072bd4519f4e93b874fe35
Kamurasi Jordan Arthur <jordan.kamurasi@anfaengineering.com>
2025-07-23 07:46:09 +0300

    Migration Reverting

---
+++ bt-remote-redesign/GNU ARM v12.2.1 - Default/gecko_sdk_4.4.6/platform/common/src/sl_syscalls.d
@@ -0,0 +1,8 @@
+gecko_sdk_4.4.6/platform/common/src/sl_syscalls.o: \
+ C:/SiliconLabs/SimplicityStudio/v5/offline/sdk/gecko-sdk/platform/common/src/sl_syscalls.c \
+ c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
+ c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\cmsis\core\include\cmsis_compiler.h \
+ c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\cmsis\core\include\cmsis_gcc.h
+c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
+c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\cmsis\core\include\cmsis_compiler.h:
+c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\cmsis\core\include\cmsis_gcc.h:
---
+++ bt_spc5x-redesign/GNU ARM v12.2.1 - Default/QEP-ONLY/qep_msm.d
@@ -0,0 +1,12 @@
+QEP-ONLY/qep_msm.o: ../QEP-ONLY/qep_msm.c \
+ c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
+ C:\qp\qpc/ports/qep-only/qp_port.h \
+ C:\Users\Admin\SimplicityStudio\v5_workspace\bt_spc5x-redesign/Inc/qp_config.h \
+ C:\qp\qpc/include/qp.h C:\qp\qpc/include/qsafe.h \
+ C:\qp\qpc/include/qs_dummy.h
+c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
+C:\qp\qpc/ports/qep-only/qp_port.h:
+C:\Users\Admin\SimplicityStudio\v5_workspace\bt_spc5x-redesign/Inc/qp_config.h:
+C:\qp\qpc/include/qp.h:
+C:\qp\qpc/include/qsafe.h:
+C:\qp\qpc/include/qs_dummy.h:
