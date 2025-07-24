25759efc1d1c29c28d107126f260f81a945541d6
Kamurasi Jordan Arthur <jordan.kamurasi@anfaengineering.com>
2025-07-24 18:44:38 +0300

    Firmware upload initial code

---
+++ bt_spc5x/GNU ARM v12.2.1 - Default/gecko_sdk_4.4.6/platform/bootloader/app_properties/app_properties.d
@@ -0,0 +1,10 @@
+gecko_sdk_4.4.6/platform/bootloader/app_properties/app_properties.o: \
+ C:/SiliconLabs/SimplicityStudio/v5/offline/sdk/gecko-sdk/platform/bootloader/app_properties/app_properties.c \
+ c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
+ C:\Users\Admin\SimplicityStudio\v5_workspace\bt_spc5x\config/app_properties_config.h \
+ C:\Users\Admin\SimplicityStudio\v5_workspace\bt_spc5x\autogen/sl_application_type.h \
+ c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\bootloader\api\application_properties.h
+c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
+C:\Users\Admin\SimplicityStudio\v5_workspace\bt_spc5x\config/app_properties_config.h:
+C:\Users\Admin\SimplicityStudio\v5_workspace\bt_spc5x\autogen/sl_application_type.h:
+c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\bootloader\api\application_properties.h:
---
+++ bt_spc5x/GNU ARM v12.2.1 - Default/gecko_sdk_4.4.6/platform/service/system/src/sl_system_init.d
@@ -0,0 +1,6 @@
+gecko_sdk_4.4.6/platform/service/system/src/sl_system_init.o: \
+ C:/SiliconLabs/SimplicityStudio/v5/offline/sdk/gecko-sdk/platform/service/system/src/sl_system_init.c \
+ c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
+ C:\Users\Admin\SimplicityStudio\v5_workspace\bt_spc5x\autogen/sl_event_handler.h
+c:\siliconlabs\simplicitystudio\v5\offline\sdk\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
+C:\Users\Admin\SimplicityStudio\v5_workspace\bt_spc5x\autogen/sl_event_handler.h:
